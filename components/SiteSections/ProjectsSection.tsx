691b31d9100d2ed2e11aca283310dca485dadacf
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-03 14:05:38 +0100

    Added new project section for AnnounceIt!

--- components/SiteSections/ProjectsSection.tsx
+++ components/SiteSections/ProjectsSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { SiteSection } from '../../styles/stitches/SiteSection_styles';
+import AnnounceItProject from '../individual_projects/AnnounceItProject';
 import PhotographyProject from '../individual_projects/PhotographyProject';
 import ReelTalk from '../individual_projects/ReelTalk';
 import WhatsPlaying from '../individual_projects/WhatsPlaying';
@@ -8,6 +9,7 @@ import WhatsPlaying from '../individual_projects/WhatsPlaying';
 const ProjectsSection: React.FC = (): JSX.Element => {
   return (
     <SiteSection id="Page3" variant="ProjectSection">
+      <AnnounceItProject />
       <PhotographyProject />
       <WhatsPlaying />
       <ReelTalk />
