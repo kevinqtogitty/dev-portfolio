3b75bae5c11b6b273d45ec457c730e7f1de80d7b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-17 18:38:47 +0100

    -

--- src/site_sections/ProjectsSection.tsx
+++ src/site_sections/ProjectsSection.tsx
@@ -302,6 +302,7 @@ const LoadingPercentage = styled(animated.span, {
   justifySelf: 'center',
   fontSize: '5rem',
   fontFamily: 'syncopate',
+  border: '2px solid red',
 });
 
 const Card = styled(animated.div, {
